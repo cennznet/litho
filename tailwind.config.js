af4bdd3d450e998f3613884e4259ff03e8d56191
Shadab Khan <shadab.2005@gmail.com>
2021-06-13 00:26:19 +0530

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ["./**/*.html", "./**/*.tsx"],
+  theme: {
+    textColor: {
+      primary: "#0070f3",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
