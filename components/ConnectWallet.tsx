9d731ffbd9992e91679977cf6e794539dc4380b2
Shadab Khan <shadab.2005@gmail.com>
2021-06-26 20:34:20 +0530

    Add wallet modal

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import Web3Context from "./Web3Context";
 import Text from "./Text";
+import Modal from "./Modal";
 
 const ConnectWallet: React.FC<{}> = () => {
   const web3Context = React.useContext(Web3Context);
   const [showUserAccount, setShowUserAccount] = React.useState(false);
+  const [showWallet, setShowWallet] = React.useState(false);
 
   React.useEffect(() => {
     if (web3Context.account) {
@@ -15,28 +17,57 @@ const ConnectWallet: React.FC<{}> = () => {
     }
   }, [web3Context.account]);
 
-  return showUserAccount && web3Context.account ? (
-    <div
-      className="border border-litho-wallet ml-9 flex items-center flex-1 pl-1 pr-2 py-2 w-40"
-      onClick={() => {
-        web3Context.connectWallet();
-      }}
-    >
-      <div className="w-2 h-2 rounded-full bg-green-500 ml-1 mr-4" />
-      {web3Context.account.meta.name}
-    </div>
-  ) : (
-    <button
-      className="border border-litho-wallet ml-9 flex items-center justify-between flex-1 pl-1 pr-2 py-2"
-      onClick={() => {
-        web3Context.connectWallet();
-      }}
-    >
-      <img src="/wallet.svg" alt="Connect Wallet" className="mr-4" />
-      <Text variant="button" color="litho-blue">
-        Connect Wallet
-      </Text>
-    </button>
+  return (
+    <>
+      <button
+        className="border border-litho-wallet ml-9 flex items-center flex-1 pl-1 pr-3 py-2 w-40"
+        onClick={() => {
+          // web3Context.connectWallet();
+          setShowWallet(true);
+        }}
+        disabled={showUserAccount && web3Context.account}
+      >
+        <img src="/wallet.svg" alt="Connect Wallet" className="mr-2" />
+        <Text
+          variant="button"
+          color="litho-blue"
+          className="flex-1 text-center"
+        >
+          {showUserAccount && web3Context.account
+            ? "Connected"
+            : "Connect Wallet"}
+        </Text>
+      </button>
+      {showWallet && (
+        <Modal
+          onClose={() => setShowWallet(false)}
+          styles={{
+            modalBody: "w-2/6 bg-white absolute top-20 right-20",
+          }}
+        >
+          <Text component="h4" variant="h4" color="litho-blue">
+            Connect Wallet
+          </Text>
+          <Text variant="body1" className="mt-4" component="div">
+            Connect with one of our available wallet providers or create a new
+            one.
+          </Text>
+          <div className="h-0.5 w-full my-6 bg-litho-black bg-opacity-10" />
+          <button
+            className="border border-litho-black border-opacity-20 flex items-center justify-center bg-litho-cream w-full py-3 h-12"
+            onClick={() => {
+              web3Context.connectWallet();
+              setShowWallet(false);
+            }}
+          >
+            <img src="/cennznet-logo.svg" className="mr-2" />
+            <Text variant="button" color="litho-blue">
+              CENNZnet Wallet
+            </Text>
+          </button>
+        </Modal>
+      )}
+    </>
   );
 };
 
