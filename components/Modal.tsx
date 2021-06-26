ac67634cd81a15f548b2aeb54cd44c2e1a86e9aa
Shadab Khan <shadab.2005@gmail.com>
2021-06-26 20:34:03 +0530

    Add modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+interface Props {
+  onClose: () => void;
+  styles?: {
+    modalBody?: string;
+    modalContainer?: string;
+  };
+}
+
+const Modal: React.FC<React.PropsWithChildren<Props>> = ({
+  onClose,
+  styles,
+  children,
+}) => {
+  const outsideClickHandler: React.EventHandler<React.SyntheticEvent> = (
+    event: React.SyntheticEvent
+  ) => {
+    const { currentTarget, target } = event;
+
+    if (currentTarget.id === (target as HTMLElement).id) {
+      onClose();
+    }
+  };
+
+  React.useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <div
+      className={`fixed top-0 left-0 h-full w-full bg-litho-cream bg-opacity-75 flex items-center justify-center ${
+        styles ? styles.modalContainer : ""
+      }`}
+      onClick={outsideClickHandler}
+      id="modal-container"
+    >
+      <div className={`absolute py-10 px-6 ${styles ? styles.modalBody : ""}`}>
+        <div className="absolute top-5 right-5" onClick={onClose}>
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
+              fill="#191B1D"
+              fillOpacity="0.6"
+            />
+          </svg>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
