83245ad615b5e1db4c2f264934291f35cb7b0b1e
Shadab Khan <shadab.2005@gmail.com>
2021-07-04 01:56:40 +0530

    Add basic my nfts page

--- pages/me.tsx
+++ pages/me.tsx
@@ -4,6 +4,11 @@ import Link from "next/link";
 import Text from "../components/Text";
 
 const Me: React.FC<{}> = () => {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     <div className="border border-litho-black mt-7 mb-6 flex flex-col">
       <Text variant="h3" component="h3" className="text-center py-4">
@@ -28,18 +33,29 @@ const Me: React.FC<{}> = () => {
         </Text>
       </div>
       <div className="p-12 overflow-auto relative min-h-customScreen2">
-        <div className="flex-1 w-full flex flex-col items-center justify-center pt-32">
-          <Text variant="h4" component="h4" className="mb-11">
-            You don't have any NFT yet.
-          </Text>
-          <Link href="/create">
-            <a className="bg-litho-blue py-3 w-80 text-center">
-              <Text variant="button" color="white">
-                Create an NFT
-              </Text>
-            </a>
-          </Link>
-        </div>
+        {!loading && true ? (
+          <div className="flex-1 w-full flex flex-col items-center justify-center pt-32">
+            <Text variant="h4" component="h4" className="mb-11">
+              You don't have any NFT yet.
+            </Text>
+            <Link href="/create">
+              <a className="bg-litho-blue py-3 w-80 text-center">
+                <Text variant="button" color="white">
+                  Create an NFT
+                </Text>
+              </a>
+            </Link>
+          </div>
+        ) : (
+          <div className="pt-16 grid grid-cols-4 gap-3">
+            <div className="h-40 bg-gray-500 rounded"></div>
+            <div className="h-40 bg-gray-500 rounded"></div>
+            <div className="h-40 bg-gray-500 rounded"></div>
+            <div className="h-40 bg-gray-500 rounded"></div>
+            <div className="h-40 bg-gray-500 rounded"></div>
+            <div className="h-40 bg-gray-500 rounded"></div>
+          </div>
+        )}
       </div>
     </div>
   );
