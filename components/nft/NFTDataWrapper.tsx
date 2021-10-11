18b82e125abd94b682a573ccd0a09ec23e70be4b
KarishmaBothara <bothara.karishma@gmail.com>
2021-10-11 15:58:48 +1300

    check now

--- components/nft/NFTDataWrapper.tsx
+++ components/nft/NFTDataWrapper.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useSWR from "swr";
 import IPFSGatewayTools from "@pinata/ipfs-gateway-tools/dist/browser";
+import axios from "axios";
 const gatewayTools = new IPFSGatewayTools();
 
 const NFTDataWrapper: React.FC<{
@@ -20,57 +21,61 @@ const NFTDataWrapper: React.FC<{
       : null
   );
 
-  const { data, error } = useSWR(metadataUrl);
-
   React.useEffect(() => {
     if (!nft.metadata) {
       console.log("NFT is::", nft);
       setError("Metadata not found");
     }
 
-    if (data) {
-      const metadata = {
-        name: data.name,
-        description: data.description,
-        image: data.image.startsWith("ipfs://")
-          ? gatewayTools.convertToDesiredGateway(
-              data.image,
+    axios
+      .get(metadataUrl)
+      .then(function (response) {
+        const { data } = response;
+        if (data) {
+          const metadata = {
+            name: data.name,
+            description: data.description,
+            image: data.image.startsWith("ipfs://")
+              ? gatewayTools.convertToDesiredGateway(
+                  data.image,
+                  process.env.NEXT_PUBLIC_PINATA_GATEWAY
+                )
+              : data.image,
+            copies: (data.properties && data.properties.quantity) || 1,
+            owner: data.properties && data.properties.owner,
+            file:
+              data.properties && data.properties.file
+                ? gatewayTools.convertToDesiredGateway(
+                    data.properties.file,
+                    process.env.NEXT_PUBLIC_PINATA_GATEWAY
+                  )
+                : null,
+            extension: data.properties && data.properties.extension,
+            coverFileExtension:
+              data.properties && data.properties.coverFileExtension,
+            metadata: gatewayTools.convertToDesiredGateway(
+              nft.metadata,
               process.env.NEXT_PUBLIC_PINATA_GATEWAY
-            )
-          : data.image,
-        copies: (data.properties && data.properties.quantity) || 1,
-        owner: data.properties && data.properties.owner,
-        file:
-          data.properties && data.properties.file
-            ? gatewayTools.convertToDesiredGateway(
-                data.properties.file,
-                process.env.NEXT_PUBLIC_PINATA_GATEWAY
+            ),
+          };
+          setNFTData({ ...nft, ...metadata });
+        }
+      })
+      .catch((error) => {
+        if (error) {
+          if (nft.metadata.startsWith("ipfs://")) {
+            setMetadataUrl(
+              gatewayTools.convertToDesiredGateway(
+                nft.metadata,
+                "https://ik.imagekit.io/i4ryln6htzn"
               )
-            : null,
-        extension: data.properties && data.properties.extension,
-        coverFileExtension:
-          data.properties && data.properties.coverFileExtension,
-        metadata: gatewayTools.convertToDesiredGateway(
-          nft.metadata,
-          process.env.NEXT_PUBLIC_PINATA_GATEWAY
-        ),
-      };
-      setNFTData({ ...nft, ...metadata });
-    }
-
-    if (error) {
-      if (nft.metadata.startsWith("ipfs://")) {
-        setMetadataUrl(
-          gatewayTools.convertToDesiredGateway(
-            nft.metadata,
-            "https://ik.imagekit.io/i4ryln6htzn"
-          )
-        );
-      } else {
-        setError(error.message);
-      }
-    }
-  }, [data, error]);
+            );
+          } else {
+            setError(error.message);
+          }
+        }
+      });
+  }, [metadataUrl]);
 
   if (!nftData.image && !nftError) {
     return null;
