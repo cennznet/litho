bb75c6ac9f219135c3bf4290bc019f8ba922a422
KarishmaBothara <bothara.karishma@gmail.com>
2021-10-15 16:41:51 +1300

    Royalty and date and USD conversion (#80)
    * Royalty and data and USD conversion
    
    * guard against nan

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 interface Props {
+  onChange?: (val) => void;
   name: string;
   type?: string;
   placeholder?: string;
@@ -15,11 +16,13 @@ interface Props {
 const Input: React.FC<Props> = ({
   type = "text",
   className,
+  onChange = (val) => {},
   ...inputProps
 }) => {
   return (
     <input
       type={type}
+      onChange={onChange}
       className={`border border-litho-black text-base p-4 ${className}`}
       {...inputProps}
     />
