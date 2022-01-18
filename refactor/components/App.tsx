180b2c85ac82b1e02853b72269e5cd6f3446eca8
Ken Vu <ken.vu@centrality.ai>
2022-01-18 13:53:09 +1300

    Port over the SupportedAssetsProvider

--- refactor/components/App.tsx
+++ refactor/components/App.tsx
@@ -1,16 +1,19 @@
 import Head from "next/head";
 
 import CENNZApiProvider from "@refactor/providers/CENNZApiProvider";
+import SupportedAssetsProvider from "@refactor/providers/SupportedAssetsProvider";
 
 export default function App({ children }) {
 	return (
 		<CENNZApiProvider endpoint={process.env.NEXT_PUBLIC_CENNZ_API_ENDPOINT}>
-			<Head>
-				<title>Litho</title>
-				<link rel="shortcut icon" href="/favicon.ico" />
-				<link rel="stylesheet" href="https://use.typekit.net/txj7ase.css" />
-			</Head>
-			{children}
+			<SupportedAssetsProvider>
+				<Head>
+					<title>Litho</title>
+					<link rel="shortcut icon" href="/favicon.ico" />
+					<link rel="stylesheet" href="https://use.typekit.net/txj7ase.css" />
+				</Head>
+				{children}
+			</SupportedAssetsProvider>
 		</CENNZApiProvider>
 	);
 }
---
+++ refactor/providers/SupportedAssetsProvider.tsx
@@ -0,0 +1,62 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useCENNZApi } from "@refactor/providers/CENNZApiProvider";
+import { u8aToString } from "@polkadot/util";
+
+const assetIds =
+  process.env.NEXT_PUBLIC_SUPPORTED_ASSETS &&
+  process.env.NEXT_PUBLIC_SUPPORTED_ASSETS.split(",");
+
+type AssetInfo = {
+  id: number;
+  symbol: string;
+  decimals: number;
+};
+
+const SupportedAssetsContext = createContext<AssetInfo[]>([]);
+
+type ProviderProps = {};
+
+export default function SupportedAssetsProvider({
+  children,
+}: PropsWithChildren<ProviderProps>) {
+  const [supportedAssets, setSupportedAssets] = useState<AssetInfo[]>();
+  const cennzApi = useCENNZApi();
+
+  useEffect(() => {
+    if (!cennzApi) return;
+    async function fetchSupportedAssets() {
+      const assets = await cennzApi.rpc.genericAsset.registeredAssets();
+      if (!assets?.length) return;
+      const assetInfos = assetIds.map((assetId) => {
+        const [tokenId, { symbol, decimalPlaces }] = assets.find(
+          (asset) => asset[0].toString() === assetId
+        );
+        return {
+          id: Number(tokenId),
+          symbol: u8aToString(symbol),
+          decimals: decimalPlaces.toNumber(),
+        };
+      });
+
+      setSupportedAssets(assetInfos);
+    }
+
+    fetchSupportedAssets();
+  }, [cennzApi, setSupportedAssets]);
+
+  return (
+    <SupportedAssetsContext.Provider value={supportedAssets}>
+      {children}
+    </SupportedAssetsContext.Provider>
+  );
+}
+
+export function useAssetInfos(): Array<AssetInfo> {
+  return useContext(SupportedAssetsContext);
+}
