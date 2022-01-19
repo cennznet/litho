25147a7e626530bfebafb853380894d41accf956
Ken Vu <ken.vu@centrality.ai>
2022-01-19 15:31:30 +1300

    Lazy load `@polkadot/extension-dapp` to avoid `window is undefined` issue

--- refactor/components/App.tsx
+++ refactor/components/App.tsx
@@ -1,10 +1,10 @@
+import { PropsWithChildren } from "react";
+import Modal from "react-modal";
 import Head from "next/head";
-
 import CENNZApiProvider from "@refactor/providers/CENNZApiProvider";
 import SupportedAssetsProvider from "@refactor/providers/SupportedAssetsProvider";
 import SupportedWalletProvider from "@refactor/providers/SupportedWalletProvider";
-import { PropsWithChildren } from "react";
-import Modal from "react-modal";
+import CENNZExtensionProvider from "@refactor/providers/CENNZExtensionProvider";
 
 Modal.setAppElement("#__next");
 
@@ -12,17 +12,22 @@ type ComponentProps = {};
 
 export default function App({ children }: PropsWithChildren<ComponentProps>) {
 	return (
-		<CENNZApiProvider endpoint={process.env.NEXT_PUBLIC_CENNZ_API_ENDPOINT}>
-			<SupportedAssetsProvider>
-				<SupportedWalletProvider>
-					<Head>
-						<title>Litho</title>
-						<link rel="shortcut icon" href="/favicon.ico" />
-						<link rel="stylesheet" href="https://use.typekit.net/txj7ase.css" />
-					</Head>
-					{children}
-				</SupportedWalletProvider>
-			</SupportedAssetsProvider>
-		</CENNZApiProvider>
+		<CENNZExtensionProvider>
+			<CENNZApiProvider endpoint={process.env.NEXT_PUBLIC_CENNZ_API_ENDPOINT}>
+				<SupportedAssetsProvider>
+					<SupportedWalletProvider>
+						<Head>
+							<title>Litho</title>
+							<link rel="shortcut icon" href="/favicon.ico" />
+							<link
+								rel="stylesheet"
+								href="https://use.typekit.net/txj7ase.css"
+							/>
+						</Head>
+						{children}
+					</SupportedWalletProvider>
+				</SupportedAssetsProvider>
+			</CENNZApiProvider>
+		</CENNZExtensionProvider>
 	);
 }
