1386f2d97d96c28e0c944361d2a3cf4512a4753d
Ken Vu <ken.vu@centrality.ai>
2022-01-24 16:44:24 +1300

    Fix up code-formatting

--- refactor/providers/SupportedAssetsProvider.tsx
+++ refactor/providers/SupportedAssetsProvider.tsx
@@ -1,21 +1,21 @@
 import {
-  createContext,
-  PropsWithChildren,
-  useContext,
-  useEffect,
-  useState,
+	createContext,
+	PropsWithChildren,
+	useContext,
+	useEffect,
+	useState,
 } from "react";
 import { useCENNZApi } from "@refactor/providers/CENNZApiProvider";
 import { u8aToString } from "@polkadot/util";
 
 const assetIds =
-  process.env.NEXT_PUBLIC_SUPPORTED_ASSETS &&
-  process.env.NEXT_PUBLIC_SUPPORTED_ASSETS.split(",");
+	process.env.NEXT_PUBLIC_SUPPORTED_ASSETS &&
+	process.env.NEXT_PUBLIC_SUPPORTED_ASSETS.split(",");
 
 export type AssetInfo = {
-  id: number;
-  symbol: string;
-  decimals: number;
+	id: number;
+	symbol: string;
+	decimals: number;
 };
 
 const SupportedAssetsContext = createContext<AssetInfo[]>([]);
@@ -23,40 +23,40 @@ const SupportedAssetsContext = createContext<AssetInfo[]>([]);
 type ProviderProps = {};
 
 export default function SupportedAssetsProvider({
-  children,
+	children,
 }: PropsWithChildren<ProviderProps>) {
-  const [supportedAssets, setSupportedAssets] = useState<AssetInfo[]>();
-  const api = useCENNZApi();
+	const [supportedAssets, setSupportedAssets] = useState<AssetInfo[]>();
+	const api = useCENNZApi();
 
-  useEffect(() => {
-    if (!api) return;
-    async function fetchSupportedAssets() {
-      const assets = await (api.rpc as any).genericAsset.registeredAssets();
-      if (!assets?.length) return;
-      const assetInfos = assetIds.map((assetId) => {
-        const [tokenId, { symbol, decimalPlaces }] = assets.find((asset) => {
-          return asset[0].toString() === assetId;
-        });
-        return {
-          id: Number(tokenId),
-          symbol: u8aToString(symbol),
-          decimals: decimalPlaces.toNumber(),
-        };
-      });
+	useEffect(() => {
+		if (!api) return;
+		async function fetchSupportedAssets() {
+			const assets = await (api.rpc as any).genericAsset.registeredAssets();
+			if (!assets?.length) return;
+			const assetInfos = assetIds.map((assetId) => {
+				const [tokenId, { symbol, decimalPlaces }] = assets.find((asset) => {
+					return asset[0].toString() === assetId;
+				});
+				return {
+					id: Number(tokenId),
+					symbol: u8aToString(symbol),
+					decimals: decimalPlaces.toNumber(),
+				};
+			});
 
-      setSupportedAssets(assetInfos);
-    }
+			setSupportedAssets(assetInfos);
+		}
 
-    fetchSupportedAssets();
-  }, [api]);
+		fetchSupportedAssets();
+	}, [api]);
 
-  return (
-    <SupportedAssetsContext.Provider value={supportedAssets}>
-      {children}
-    </SupportedAssetsContext.Provider>
-  );
+	return (
+		<SupportedAssetsContext.Provider value={supportedAssets}>
+			{children}
+		</SupportedAssetsContext.Provider>
+	);
 }
 
 export function useAssets(): Array<AssetInfo> {
-  return useContext(SupportedAssetsContext);
+	return useContext(SupportedAssetsContext);
 }
