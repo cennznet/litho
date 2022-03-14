45d3c78337a410d0c62895239d572d2d87a43589
Ken Vu <ken.vu@centrality.ai>
2022-03-14 13:53:38 +1300

    Remove old components folder

--- components/DeviceContext.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DeviceContext = React.createContext({
-  isMobile: null,
-  isChrome: true,
-  isFirefox: true,
-});
-
-export default DeviceContext;
--- components/nft/index.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from "next/dynamic";
-
-const NFT = dynamic(import("./NFTDataWrapper"), { ssr: false });
-
-export default NFT;
