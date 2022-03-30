5b84ddac0e5c400009cc79a3a936928b628f763a
Ken Vu <ken.vu@centrality.ai>
2022-03-30 22:06:06 +1300

    Fix build issue

--- refactor/utils/parseDescriptionForViewStoryLink.ts
+++ refactor/utils/parseDescriptionForViewStoryLink.ts
@@ -3,9 +3,9 @@ export default function parseDescriptionForViewStoryLink(
 ): [string, string, string] {
 	const regex =
 		/(\shere)*:\s[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-	const matches = [...description.matchAll(regex)];
+	const matches = [...Array.from(description.matchAll(regex))];
 
-	if (!matches?.length) return [description, null];
+	if (!matches?.length) return [description, null, null];
 
 	const [match0] = matches[0];
 
