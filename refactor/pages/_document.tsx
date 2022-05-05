e50b749c99ed92d4e703fa5bea7e1bfd6d593abe
aidan-starke <starke.aidan@gmail.com>
2022-05-05 15:11:31 +1200

    setup `prettier-format`, add `.idea` to gitIgnore

--- refactor/pages/_document.tsx
+++ refactor/pages/_document.tsx
@@ -3,19 +3,19 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 const gaID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export class Document extends NextDocument {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {!!gaID && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${gaID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
+	render() {
+		return (
+			<Html>
+				<Head>
+					{!!gaID && (
+						<>
+							<script
+								async
+								src={`https://www.googletagmanager.com/gtag/js?id=${gaID}`}
+							/>
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
@@ -23,16 +23,16 @@ export class Document extends NextDocument {
                 page_path: window.location.pathname,
               });
             `,
-                }}
-              />
-            </>
-          )}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+								}}
+							/>
+						</>
+					)}
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
